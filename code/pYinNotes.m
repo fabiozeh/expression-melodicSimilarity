6e3f91c4f46630d9cd593ba0757937d2b86a36ac
Fabio <fabiojmortega@yahoo.com.br>
2017-03-23 18:46:41 +0100

    Corrections for older Matlab compatibility and automatic folder creation.

--- code/pYinNotes.m
+++ code/pYinNotes.m
@@ -2,11 +2,11 @@
 % from the pYin VAMP plugin, by Matthias Mauch.
 % The VAMP plugin is invoked directly using Sonic Annotator.
 % The output is a midi file with the same name as the input wav file.
-function pYinNotes(wavFileFolder, wavFileName, outputFilePath, pYinPath, sonicAnnotatorPath, transformFilePath)
+function pYinNotes(wavFileFolder, wavFileName, outputFilePath, outputFileName, pYinPath, sonicAnnotatorPath, transformFilePath)
 
 if isunix(), sep = '/'; else sep = '\'; end
 
-if nargin < 4
+if nargin < 5
     pYinPath = ['..' sep 'resources' sep 'pYin'];
     sonicAnnotatorPath = ['..' sep 'resources' sep 'sonic-annotator'];
     transformFilePath = [sonicAnnotatorPath sep 'transform.rdf'];
@@ -16,6 +16,7 @@ if isunix
     % set VAMP_PATH to include pYinPath
     exportCommand = ['export VAMP_PATH=$(pwd)/' pYinPath '; '];
     moveCommand = 'mv ';
+    mdCommand = 'mkdir -p ';
     if ismac
         platFolder = 'macosx';
     else
@@ -24,12 +25,16 @@ if isunix
 else
     exportCommand = ['set VAMP_PATH=%cd%\' pYinPath ' & '];
     moveCommand = 'move ';
+    mdCommand = 'md ';
     platFolder = 'win32';
 end
 system([exportCommand sonicAnnotatorPath sep platFolder sep 'sonic-annotator -t ' ...
      transformFilePath ' -w midi ' wavFileFolder sep wavFileName]);
 
 if nargin >= 3
+    if ~exist(outputFilePath, 'file')
+        system([mdCommand outputFilePath]);
+    end
     system([moveCommand wavFileFolder sep 'performance.mid ' ...
-        outputFilePath]);
+        outputFilePath sep outputFileName]);
 end
